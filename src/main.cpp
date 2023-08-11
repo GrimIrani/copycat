16a099e90f7c4de0fd2c4861fa1d161084ed2358
GrimIrani <GrimIrani@gmail.com>
2023-08-12 03:07:43 +0330

    Add tray icon(DEMO)

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,24 @@
 
 #include <QApplication>
 #include <QClipboard>
+#include <QSystemTrayIcon>
 
 int main(int argc, char *argv[]){
   QApplication app(argc, argv);
+
+  // Create a system tray icon object
+  QSystemTrayIcon trayIcon;
+
+  // Customize the icon and tooltip as needed
+  QIcon icon("../res/icon.png");
+  trayIcon.setIcon(icon);
+  trayIcon.setToolTip("copycat");
+  trayIcon.show();
+
   QClipboard *clipboard = QGuiApplication::clipboard();
   QString originalText = clipboard->text();
 
-  //Without arg
+   // Without arg
   if(argv[1] == NULL){
     QString QLine;
     std::string Line;
@@ -17,11 +28,11 @@ int main(int argc, char *argv[]){
       QLine += QString::fromStdString(Line)+"\n";
     QLine.chop(1);
     clipboard->setText(QLine);
-  }
+   }
 
-  //With arg
+   // With arg
   else{
-    clipboard->setText(argv[1]);
+     clipboard->setText(argv[1]);
   }
   return app.exec();
 }
